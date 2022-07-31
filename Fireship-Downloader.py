2cc07ece1bfe4e29455d89f65de25548a8460702
Tamizh Arasan <no.1opensourcelover@protonmail.com>
2022-07-31 23:30:36 +0530

    FireshipDownloader: Initial Commit

---
+++ Fireship-Downloader.py
@@ -0,0 +1,100 @@
+# This is a fireship course downloader made by https://telegram.dog/fosslover
+# which is used to download courses from https://fireship.io (You can download pro courses too XD)
+# You can check out my github account at https://github.com/fosslover69
+# This script relies on yt-dlp use `pip install yt-dlp` to install it
+# Support: https://telegram.dog/fossaf
+
+from re import findall
+import subprocess
+import urllib.request
+import os
+
+print("""
+\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+\t\tFireship Course Downloader
+\t\t\t\t\t- @fosslover
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~""")
+
+while(True):
+    try:
+        userMenuChoice=int(input("""
+1. Download Course Here
+2. Download Course in Seperate Folder
+Enter Your Choice: """))
+        if userMenuChoice != 1 and userMenuChoice != 2:
+            raise ValueError
+        break
+    except (ValueError):
+        print("Enter a Valid Choice")
+    except (KeyboardInterrupt):
+        print("\nExiting.....")
+        exit()
+
+while(True):
+    try:
+        courseLinkInput = input("""
+Enter the Fireship Course Link (Multiple links are Supported Eg. link1 link2 ): """)
+        # Spliting the links if multiple links are given
+        courseLinkList = courseLinkInput.split(" ")
+        for courseLink in courseLinkList:
+            courseLink = courseLink.strip()
+            # Checking if the given links are from fireship.io
+            if "fireship.io" not in courseLink:
+                raise ValueError
+            # Checking if the given links are valid
+            fireshipResponse = urllib.request.urlopen(courseLink).read().decode("utf-8")
+        break
+    except (ValueError):
+        print("\nEnter a Valid Fireship.io link")
+    except (KeyboardInterrupt):
+        print("\nExiting.....")
+        exit()
+# Looping through the provided links
+for courseLink in courseLinkList:
+    # Striping the link incase if there's duplicated whitespace
+    courseLink = courseLink.strip()
+    # Connecting to the url and fetching the HTML
+    fireshipResponse = urllib.request.urlopen(courseLink).read().decode("utf-8")
+    # Parsing and Formatting the obtained HTML response
+    fireshipResponse = fireshipResponse.split("\n")
+    stripedResponse=[]
+    for element in fireshipResponse:
+        element = element.strip()
+        stripedResponse.append(element)
+
+    # Parsing through the <header> tag to fetch the course title
+    courseHeader = findall('<header>(.*?)</header>', str(stripedResponse))
+    courseTitle = findall('id="(.*?)"', str(courseHeader))
+    courseTitle = courseTitle[0].capitalize()
+    
+    # Fetching all the video links in the course 
+    url=[]
+    for line in stripedResponse:
+        line=line.strip()
+        if line.startswith('<a href="/courses/'):
+            line=findall(r'"(.*?)"', line)
+            url.append(line)
+    linkList=[]
+    for line in url:
+        for link in line:
+            link="https://fireship.io/"+link
+            linkList.append(link)
+    
+    # Storing the Links in a Textfile to make batch process with yt-dlp
+    fireshipLinkOut=open(courseTitle+".txt", "w")
+    
+    for link in linkList:
+        fireshipLinkOut.write(link+"\n")
+    fireshipLinkOut.close()
+    
+    # Downloading the Lessons with yt-dlp according to the users choice
+    if userMenuChoice==1:
+        subprocess.run(["yt-dlp","-f","mp4","-a",courseTitle+".txt"])
+    if userMenuChoice==2:
+        os.makedirs(courseTitle,exist_ok=True)
+        subprocess.run(["yt-dlp","-f","mp4","-a",courseTitle+".txt","-P",courseTitle])
+
+# Cleaning up text files created for batch operations
+for fileName in os.listdir():
+    if fileName.endswith(".txt"):
+        os.remove(fileName)
---
+++ README.md
@@ -0,0 +1,10 @@
+# Fireship Downloader
+A simple video downloader script to download courses from [fireship.io](https://fireship.io) with yt-dlp
+
+You can download pro courses too XD
+
+This script relies on yt-dlp use `pip install yt-dlp` to install it
+
+To download courses do `python3 Fireship-Downloader.py` and follow the On-Screen instructions 
+ 
+Support Group: [Telegram](https://telegram.dog/fossaf)
