30ce38cf03765084b18973568bdfb256c6fe2f22
Shivam Kumar <kumar.shivam.jarvis@gmail.com>
2022-08-11 18:27:30 +0530

    Fix lesson downloading and make download_course function
    Signed-off-by: Shivam Kumar <kumar.shivam.jarvis@gmail.com>

--- Fireship-Downloader.py
+++ Fireship-Downloader.py
@@ -60,6 +60,110 @@ def download_video(link, filename):
         ydl.download([link])
 
 
+def download_course(course_link):
+    session = requests.Session()
+
+    # Get url content
+    fireship_response = session.get(course_link).content
+
+    # Use beautifulsoup for parsing html
+    soup = BeautifulSoup(fireship_response, 'html.parser')
+
+    videos_links = {}
+
+    print(f"\nGetting Download Links :-")
+
+    # If link is a course
+    if "lessons" not in course_link:
+        # Get course title and sanitize it for using as file or directory
+        course_title = sanitize_filepath(soup.find("h1", attrs={"itemprop": "name"}).get_text().strip())
+        print(f"\nCourse Title - {course_title}")
+
+        # Initial Section
+        current_section = "99. Unknown Section"
+        section_counter = 1
+
+        # Iterate over every child element of chapters
+        for child in soup.find("div", class_="chapters-wrap").findChildren(recursive=False):
+            # Arrange videos by sections
+            if child.name == "h3":
+                # Sanitize section name
+                current_section = sanitize_filepath(f"{section_counter}. {child.get_text().strip()}")
+                section_counter += 1
+
+                print(f"Processing Section - {current_section}")
+
+                # Add section for video links
+                videos_links[current_section] = list()
+
+                # Current child is no longer needed
+                continue
+
+            # Add link to appropriate sections
+            elif child.name == "a":
+                # Get video name
+                file_name = sanitize_filepath(f'{child.find("strong").get_text().strip()} - {child.find("span", attrs={"class": "subtext"}).get_text().strip()}')
+                print(file_name)
+
+                # Fireship url of video
+                video_fireship_link = f"https://fireship.io{child['href']}"
+
+                # Add video info to video links
+                videos_links[current_section].append({"file_name": file_name, "fireship_link": video_fireship_link})
+
+            else:
+                # If got unexpected child tag
+                print("Unknown child element while getting links")
+                print(child.prettify())
+                print("Continuing from next elements...")
+                continue
+
+        # Download this course
+        print(f"\n\n\nStarting Download for Course - {course_title}")
+
+        # Get current directory
+        script_dir = os.getcwd()
+
+        # Make course directory
+        course_dir = os.path.join(script_dir, course_title)
+        os.makedirs(course_dir, exist_ok=True)
+
+        # Iterate over every section
+        for section_name, videos in videos_links.items():
+            print(f"\nDownloading section - {section_name}")
+
+            # Make sections directory
+            section_dir = os.path.join(course_dir, section_name)
+            os.makedirs(section_dir, exist_ok=True)
+
+            # Download every video
+            for video in videos:
+                print(f'Downloading Video - {video["file_name"]}')
+                download_video(video["fireship_link"], os.path.join(section_dir, video["file_name"]))
+
+            # Go to course directory for downloading next section
+            os.chdir(course_dir)
+
+        print(f"\n\n\nDownloaded Course - {course_title}")
+        os.chdir(script_dir)
+
+    # If the link is a single lesson
+    else:
+        # Get lesson name
+        lesson_title = sanitize_filepath(soup.find("h1", attrs={"itemprop": "name headline"}).get_text().strip())
+
+        # Add lesson info to lesson links
+        video = {"file_name": lesson_title, "fireship_link": course_link}
+
+        # Download Lesson
+        print(f'Downloading lesson - {video["file_name"]}')
+        download_video(video["fireship_link"], video["file_name"])
+        print(f"\n\n\nDownloaded Lesson - {lesson_title}")
+
+    # Close requests session
+    session.close()
+
+
 def main():
     course_links = get_course_links()
 
@@ -67,105 +171,7 @@ def main():
     for course_link in course_links:
         print(f"\n\n\nProcessing Link - {course_link}")
 
-        session = requests.Session()
-
-        # Get url content
-        fireship_response = session.get(course_link).content
-
-        # Use beautifulsoup for parsing html
-        soup = BeautifulSoup(fireship_response, 'html.parser')
-
-        # Get course title and sanitize it for using as file or directory
-        course_title = sanitize_filepath(soup.find("h1", attrs={"itemprop": "name"}).get_text().strip())
-        print(f"\nCourse Title - {course_title}")
-
-        videos_links = {}
-
-        print(f"\nGetting Download Links :-")
-
-        # If link is a course
-        if "lessons" not in course_link:
-            current_section = "99. Unknown Section"
-            section_counter = 1
-
-            # Iterate over every child element of chapters
-            for child in soup.find("div", class_="chapters-wrap").findChildren(recursive=False):
-                # Arrange videos by sections
-                if child.name == "h3":
-                    # Sanitize section name
-                    current_section = sanitize_filepath(f"{section_counter}. {child.get_text().strip()}")
-                    section_counter += 1
-
-                    print(f"Processing Section - {current_section}")
-
-                    # Add section for video links
-                    videos_links[current_section] = list()
-
-                    # Current child is no longer needed
-                    continue
-
-                # Add link to appropriate sections
-                elif child.name == "a":
-                    # Get video name
-                    file_name = sanitize_filepath(f'{child.find("strong").get_text().strip()} - {child.find("span", attrs={"class": "subtext"}).get_text().strip()}')
-                    print(file_name)
-
-                    # Fireship url of video
-                    video_fireship_link = f"https://fireship.io{child['href']}"
-
-                    # Add video info to video links
-                    videos_links[current_section].append({"file_name": file_name, "fireship_link": video_fireship_link})
-
-                else:
-                    # If got unexpected child tag
-                    print("Unknown child element while getting links")
-                    print(child.prettify())
-                    print("Continuing from next elements...")
-                    continue
-
-            # Download this course
-            print(f"\n\n\nStarting Download for Course - {course_title}")
-
-            # Get current directory
-            script_dir = os.getcwd()
-
-            # Make course directory
-            course_dir = os.path.join(script_dir, course_title)
-            os.makedirs(course_dir, exist_ok=True)
-
-            # Iterate over every section
-            for section_name, videos in videos_links.items():
-                print(f"\nDownloading section - {section_name}")
-
-                # Make sections directory
-                section_dir = os.path.join(course_dir, section_name)
-                os.makedirs(section_dir, exist_ok=True)
-
-                # Download every video
-                for video in videos:
-                    print(f'Downloading Video - {video["file_name"]}')
-                    download_video(video["fireship_link"], os.path.join(section_dir, video["file_name"]))
-
-                # Go to course directory for downloading next section
-                os.chdir(course_dir)
-
-            print(f"\n\n\nDownloaded Course - {course_title}")
-            os.chdir(script_dir)
-
-        # If the link is a single lesson
-        else:
-            # Get lesson name
-            file_name = sanitize_filepath(f'{soup.find("post-video").get_text().strip()}')
-
-            # Add lesson info to lesson links
-            video = {"file_name": file_name, "fireship_link": course_link}
-
-            # Download Lesson
-            print(f'Downloading lesson - {video["file_name"]}')
-            download_video(video["lesson_actual_link"], video["file_name"])
-
-        # Close requests session
-        session.close()
+        download_course(course_link)
 
 
 if __name__ == "__main__":
